378b3cd367546cb9931a95764e8789dda0a3dca2
Navya Malhotra <navya0807@gmail.com>
2024-05-22 00:00:29 +0530

    removal of project and orders overview cards

--- src/layouts/dashboard/components/Projects/index.js
+++ src/layouts/dashboard/components/Projects/index.js
@@ -63,49 +63,7 @@ function Projects() {
     </Menu>
   );
 
-  return (
-    <Card
-      sx={{
-        height: "100% !important",
-      }}
-    >
-      <VuiBox display="flex" justifyContent="space-between" alignItems="center" mb="32px">
-        <VuiBox mb="auto">
-          <VuiTypography color="white" variant="lg" mb="6px" gutterBottom>
-            Projects
-          </VuiTypography>
-          <VuiBox display="flex" alignItems="center" lineHeight={0}>
-            <BsCheckCircleFill color="green" size="15px" />
-            <VuiTypography variant="button" fontWeight="regular" color="text" ml="5px">
-              &nbsp;<strong>30 done</strong> this month
-            </VuiTypography>
-          </VuiBox>
-        </VuiBox>
-        <VuiBox color="text" px={2}>
-          <Icon sx={{ cursor: "pointer", fontWeight: "bold" }} fontSize="small" onClick={openMenu}>
-            more_vert
-          </Icon>
-        </VuiBox>
-        {renderMenu}
-      </VuiBox>
-      <VuiBox
-        sx={{
-          "& th": {
-            borderBottom: ({ borders: { borderWidth }, palette: { grey } }) =>
-              `${borderWidth[1]} solid ${grey[700]}`,
-          },
-          "& .MuiTableRow-root:not(:last-child)": {
-            "& td": {
-              borderBottom: ({ borders: { borderWidth }, palette: { grey } }) =>
-                `${borderWidth[1]} solid ${grey[700]}`,
-            },
-          },
-        }}
-      >
-        <Table columns={columns} rows={rows} />
-      </VuiBox>
-    </Card>
-  );
+  
 }
 
 export default Projects;
