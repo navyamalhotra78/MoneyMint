e8febe37ffa9a245298846f88993bd6b2bd9f76a
Navya Malhotra <navya0807@gmail.com>
2024-06-07 20:31:19 +0530

    removing rtl pages

--- src/routes.js
+++ src/routes.js
@@ -42,7 +42,6 @@
 import Dashboard from "layouts/dashboard";
 import Tables from "layouts/tables";
 import Billing from "layouts/billing";
-import RTL from "layouts/rtl";
 import Profile from "layouts/profile";
 import SignIn from "layouts/authentication/sign-in";
 import SignUp from "layouts/authentication/sign-up";
@@ -82,15 +81,6 @@ const routes = [
     route: "/billing",
     icon: <BsCreditCardFill size="15px" color="inherit" />,
     component: Billing,
-    noCollapse: true,
-  },
-  {
-    type: "collapse",
-    name: "RTL",
-    key: "rtl",
-    route: "/rtl",
-    icon: <IoBuild size="15px" color="inherit" />,
-    component: RTL,
     noCollapse: true,
   },
   { type: "title", title: "Account Pages", key: "account-pages" },
