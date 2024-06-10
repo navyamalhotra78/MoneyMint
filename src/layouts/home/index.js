d05dcc452222156ea31c399dcf4a4564a139cccb
Navya Malhotra <navya0807@gmail.com>
2024-06-10 14:00:06 +0530

    home page

--- src/layouts/home/index.js
+++ src/layouts/home/index.js
@@ -32,7 +32,7 @@ import { IoBuild } from "react-icons/io5";
 import { IoWallet } from "react-icons/io5";
 import { IoDocumentText } from "react-icons/io5";
 import { FaShoppingCart } from "react-icons/fa";
-
+import { Link } from 'react-router-dom';
 // Data
 import LineChart from "examples/Charts/LineCharts/LineChart";
 import BarChart from "examples/Charts/BarCharts/BarChart";
@@ -49,174 +49,75 @@ function Dashboard() {
     <DashboardLayout>
       <DashboardNavbar />
       <VuiBox py={3}>
-        
-        <VuiBox mb={3}>
-          <Grid container spacing="18px">
-            <Grid item xs={12} lg={12} xl={5}>
-              <WelcomeMark />
-            </Grid>
-            <Grid item xs={12} lg={6} xl={3}>
-              <SatisfactionRate />
-            </Grid>
-            <Grid item xs={12} lg={6} xl={4}>
-              <ReferralTracking />
-            </Grid>
+        <Grid>
+          <Grid item xs={12} md={6} xl={3} ml={42}>
+            <VuiTypography color="white" fontWeight="bold" style={{ fontSize: '50px' }}>
+              Welcome to Money Mint.
+            </VuiTypography>
           </Grid>
-        </VuiBox>
-        <VuiBox mb={3}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} lg={6} xl={7}>
-              <Card>
-                <VuiBox sx={{ height: "415px" }}>
-                  <VuiTypography variant="lg" color="white" fontWeight="bold" mb="5px">
-                    Spending Analysis- past 12 months
-                  </VuiTypography>
-                  <VuiBox display="flex" alignItems="center" mb="40px">
-                  </VuiBox>
-                  <VuiBox sx={{ height: "310px" }}>
-                    <LineChart
-                      lineChartData={lineChartDataDashboard}
-                      lineChartOptions={lineChartOptionsDashboard}
-                    />
-                  </VuiBox>
-                </VuiBox>
-              </Card>
-            </Grid>
-            <Grid item xs={12} lg={6} xl={5}>
-              <Card>
-                <VuiBox>
-                  <VuiBox
-                    mb="24px"
-                    height="220px"
-                    sx={{
-                      background: linearGradient(
-                        cardContent.main,
-                        cardContent.state,
-                        cardContent.deg
-                      ),
-                      borderRadius: "20px",
-                    }}
-                  >
-                    <BarChart
-                      barChartData={barChartDataDashboard}
-                      barChartOptions={barChartOptionsDashboard}
-                    />
-                  </VuiBox>
-                  <VuiTypography variant="lg" color="white" fontWeight="bold" mb="5px">
-                    Category Wise Expenditure
-                  </VuiTypography>
-                  <VuiBox display="flex" alignItems="center" mb="40px">
-                    <VuiTypography variant="button" color="success" fontWeight="bold">
-                      <VuiTypography variant="button" color="text" fontWeight="regular">
-                      </VuiTypography>
-                    </VuiTypography>
-                  </VuiBox>
-                  <Grid container spacing="50px">
-                    <Grid item xs={6} md={3} lg={3}>
-                      <Stack
-                        direction="row"
-                        spacing={{ sm: "10px", xl: "4px", xxl: "10px" }}
-                        mb="6px"
-                      >
-                        <VuiBox
-                          bgColor="info"
-                          display="flex"
-                          justifyContent="center"
-                          alignItems="center"
-                          sx={{ borderRadius: "6px", width: "25px", height: "25px" }}
-                        >
-                          <IoWallet color="#fff" size="12px" />
-                        </VuiBox>
-                        <VuiTypography color="text" variant="button" fontWeight="medium">
-                         Essentials
-                        </VuiTypography>
-                      </Stack>
-                      <VuiTypography color="white" variant="lg" fontWeight="bold" mb="8px">
-                        32,984
-                      </VuiTypography>
-                      <VuiProgress value={60} color="info" sx={{ background: "#2D2E5F" }} />
-                    </Grid>
-                    <Grid item xs={6} md={3} lg={3}>
-                      <Stack
-                        direction="row"
-                        spacing={{ sm: "10px", xl: "4px", xxl: "10px" }}
-                        mb="6px"
-                      >
-                        <VuiBox
-                          bgColor="info"
-                          display="flex"
-                          justifyContent="center"
-                          alignItems="center"
-                          sx={{ borderRadius: "6px", width: "25px", height: "25px" }}
-                        >
-                          <IoIosRocket color="#fff" size="12px" />
-                        </VuiBox>
-                        <VuiTypography color="text" variant="button" fontWeight="medium">
-                          Medical
-                        </VuiTypography>
-                      </Stack>
-                      <VuiTypography color="white" variant="lg" fontWeight="bold" mb="8px">
-                        2,422
-                      </VuiTypography>
-                      <VuiProgress value={60} color="info" sx={{ background: "#2D2E5F" }} />
-                    </Grid>
-                    <Grid item xs={6} md={3} lg={3}>
-                      <Stack
-                        direction="row"
-                        spacing={{ sm: "10px", xl: "4px", xxl: "10px" }}
-                        mb="6px"
-                      >
-                        <VuiBox
-                          bgColor="info"
-                          display="flex"
-                          justifyContent="center"
-                          alignItems="center"
-                          sx={{ borderRadius: "6px", width: "25px", height: "25px" }}
-                        >
-                          <FaShoppingCart color="#fff" size="12px" />
-                        </VuiBox>
-                        <VuiTypography color="text" variant="button" fontWeight="medium">
-                          Transportation
-                        </VuiTypography>
-                      </Stack>
-                      <VuiTypography color="white" variant="lg" fontWeight="bold" mb="8px">
-                        2,400
-                      </VuiTypography>
-                      <VuiProgress value={60} color="info" sx={{ background: "#2D2E5F" }} />
-                    </Grid>
-                    <Grid item xs={6} md={3} lg={3}>
-                      <Stack
-                        direction="row"
-                        spacing={{ sm: "10px", xl: "4px", xxl: "10px" }}
-                        mb="6px"
-                      >
-                        <VuiBox
-                          bgColor="info"
-                          display="flex"
-                          justifyContent="center"
-                          alignItems="center"
-                          sx={{ borderRadius: "6px", width: "25px", height: "25px" }}
-                        >
-                          <IoBuild color="#fff" size="12px" />
-                        </VuiBox>
-                        <VuiTypography color="text" variant="button" fontWeight="medium">
-                          Personal
-                        </VuiTypography>
-                      </Stack>
-                      <VuiTypography color="white" variant="lg" fontWeight="bold" mb="8px">
-                        320
-                      </VuiTypography>
-                      <VuiProgress value={60} color="info" sx={{ background: "#2D2E5F" }} />
-                    </Grid>
-                  </Grid>
-                </VuiBox>
-              </Card>
-            </Grid>
+        </Grid>
+        <Grid>
+          <Grid item xs={12} md={6} xl={3} ml={55}>
+            <VuiTypography color="white" style={{ fontSize: '20px' }}>
+              Your one stop personal financial tracker.
+            </VuiTypography>
           </Grid>
-        </VuiBox>
-        <Grid container spacing={3} direction="row" justifyContent="center" alignItems="stretch">
-          <Grid item xs={12} md={6} lg={8}>
-            <Projects />
+        </Grid>
+        <Grid>
+          <Grid item>
+            <VuiTypography color="white" style={{ fontSize: '22px' }} ml={5} mr={5} mt={5} mb={3}>
+            Financial literacy is essential in the modern world as it empowers individuals to make informed
+            decisions about managing their money, saving, and investing. Understanding financial concepts like 
+            budgeting, interest rates, and credit can help people avoid debt and achieve financial stability. 
+            In an era where financial products are increasingly complex, financial literacy enables individuals 
+            to navigate these options and plan for the future. Having a financial tracker is crucial as it 
+            provides a clear overview of income, expenses, and savings, helping individuals stay on top of 
+            their finances. This tool aids in setting and achieving financial goals, ensuring better financial 
+            health and security.
+            </VuiTypography>
+          </Grid>
+        </Grid>
+        <Grid>
+          <Grid item>
+            <VuiTypography color="white" style={{ fontSize: '22px' }} ml={5} mr={5} mt={5} mb={3}>
+              Features MoneyMint offers to help you stay on top of your finances-
+              <VuiTypography color="white" style={{ fontSize: '22px' }} ml={5} mr={5} mt={5} mb={3}>
+              <ul>
+              <li>Personalized Financial Dashboard: A centralized dashboard where users can input and track 
+                their financial goals, income, expenses, and savings preferences.
+              </li>
+              <li>Spending Analysis: Tools to analyze users' spending patterns and identify areas where they 
+                can potentially save money.
+              </li>
+              <li>Budgeting Tools: Features to help users create and stick to a budget, with alerts for 
+                overspending or upcoming bills.
+              </li>
+              <li>Financial Education: AIML chatbot to answer users' financial questions, explain financial 
+                concepts, and offer tips for improving financial literacy.
+              </li>
+              <li>Goal Tracking: Progress tracking for financial goals, with milestones and reminders to help 
+                users stay on track.
+              </li>
+              <li>Customizable Alerts: Options for users to set up customizable alerts for important financial 
+                events.
+              </li>
+              <li>Financial Calculators: Tools for calculating loan payments, savings projections, retirement 
+                needs, and other financial metrics.
+              </li>
+              <li>Document Management: Secure storage for important financial documents.
+              </li>
+              </ul>
+              </VuiTypography>
+            </VuiTypography>
+          </Grid>
+        </Grid>
+        <Grid>
+          <Grid item>
+            <Link to="/authentication/sign up/index.js">
+            <VuiTypography color="white" style={{ fontSize: '22px' }} ml={42} mr={5} mt={5} mb={3}>
+              Sign up today and get started on your journey of financial mastery!
+            </VuiTypography>
+            </Link>
           </Grid>
         </Grid>
       </VuiBox>
