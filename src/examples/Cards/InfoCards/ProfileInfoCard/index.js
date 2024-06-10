f240f53adafe1cf907f43437b137244a90fb0b7d
Navya Malhotra <navya0807@gmail.com>
2024-06-10 23:33:22 +0530

    editing for profile

--- src/examples/Cards/InfoCards/ProfileInfoCard/index.js
+++ src/examples/Cards/InfoCards/ProfileInfoCard/index.js
@@ -1,51 +1,51 @@
-// prop-types is library for typechecking of props
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-
-// @mui material components
 import Card from "@mui/material/Card";
 import Divider from "@mui/material/Divider";
-
-// Vision UI Dashboard React components
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import VuiBox from "components/VuiBox";
 import VuiTypography from "components/VuiTypography";
-
-// Vision UI Dashboard React base styles
 import colors from "assets/theme/base/colors";
 import typography from "assets/theme/base/typography";
 
 function ProfileInfoCard({ title, description, info, social }) {
-  const labels = [];
-  const values = [];
+  const [editableInfo, setEditableInfo] = useState(info);
+  const [isEditing, setIsEditing] = useState(false);
   const { size } = typography;
 
-  // Convert this form `objectKey` of the object key in to this `object key`
-  Object.keys(info).forEach((el) => {
-    if (el.match(/[A-Z\s]+/)) {
-      const uppercaseLetter = Array.from(el).find((i) => i.match(/[A-Z]+/));
-      const newElement = el.replace(uppercaseLetter, ` ${uppercaseLetter.toLowerCase()}`);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditableInfo({ ...editableInfo, [name]: value });
+  };
 
-      labels.push(newElement);
-    } else {
-      labels.push(el);
-    }
-  });
+  const handleSave = () => {
+    // Here you can handle the save functionality
+    console.log("Saved info: ", editableInfo);
+    setIsEditing(false);
+  };
 
-  // Push the object values into the values array
-  Object.values(info).forEach((el) => values.push(el));
-
-  // Render the card info items
-  const renderItems = labels.map((label, key) => (
+  const renderItems = Object.keys(editableInfo).map((label, key) => (
     <VuiBox key={label} display="flex" py={1} pr={2}>
       <VuiTypography variant="button" color="text" fontWeight="regular" textTransform="capitalize">
         {label}: &nbsp;
       </VuiTypography>
-      <VuiTypography variant="button" fontWeight="regular" color="white" >
-        &nbsp;{values[key]}
-      </VuiTypography>
+      {isEditing ? (
+        <TextField
+          variant="standard"
+          name={label}
+          value={editableInfo[label]}
+          onChange={handleChange}
+          InputProps={{ style: { color: "white" } }}
+        />
+      ) : (
+        <VuiTypography variant="button" fontWeight="regular" color="white">
+          &nbsp;{editableInfo[label]}
+        </VuiTypography>
+      )}
     </VuiBox>
   ));
 
-  // Render the card social media icons
   const renderSocial = social.map(({ link, icon, color }) => (
     <VuiBox
       key={color}
@@ -64,15 +64,14 @@ function ProfileInfoCard({ title, description, info, social }) {
   ));
 
   return (
-    <Card
-      sx={{
-        height: "100%",
-      }}
-    >
+    <Card sx={{ height: "100%" }}>
       <VuiBox display="flex" mb="14px" justifyContent="space-between" alignItems="center">
         <VuiTypography variant="lg" fontWeight="bold" color="white" textTransform="capitalize">
           {title}
         </VuiTypography>
+        <Button onClick={() => setIsEditing(!isEditing)} variant="contained" color="primary">
+          {isEditing ? "Cancel" : "Edit"}
+        </Button>
       </VuiBox>
       <VuiBox>
         <VuiBox mb={2} lineHeight={1}>
@@ -83,26 +82,23 @@ function ProfileInfoCard({ title, description, info, social }) {
         <VuiBox opacity={0.3}>
           <Divider />
         </VuiBox>
-        <VuiBox>
-          {renderItems}
-          <VuiBox display="flex" py={1} pr={2} color="white">
-            <VuiTypography
-              variant="button"
-              fontWeight="regular"
-              color="text"
-              textTransform="capitalize"
-            >
-              social: &nbsp;
-            </VuiTypography>
-            {renderSocial}
-          </VuiBox>
+        <VuiBox>{renderItems}</VuiBox>
+        <VuiBox display="flex" py={1} pr={2} color="white">
+          <VuiTypography variant="button" fontWeight="regular" color="text" textTransform="capitalize">
+            social: &nbsp;
+          </VuiTypography>
+          {renderSocial}
         </VuiBox>
+        {isEditing && (
+          <Button onClick={handleSave} variant="contained" color="primary">
+            Save
+          </Button>
+        )}
       </VuiBox>
     </Card>
   );
 }
 
-// Typechecking props for the ProfileInfoCard
 ProfileInfoCard.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
